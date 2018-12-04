42f74d9c418db8ff382063b0e1697b1d034de1b2
Johnny <johnathan.dimatteo@gmail.com>
2018-12-04 16:07:38 -0500

    Delete som.py

--- src/som.py
+++
@@ -1,7 +0,0 @@
-# from https://github.com/FlorisHoogenboom/som-anomaly-detector
-# who in turn got it from 
-# Tian, J., Azarian, M. H., Pecht, M. (2014). 
-# Anomaly detection using self-organizing maps-based K-nearest neighbour Algorithm. Proceedings of the European Conference of the Prognostics and Health Management Society.
-# MIT License
-
-# Copyright (c) 2017 Floris Hoogenboom
